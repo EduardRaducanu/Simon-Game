009df81f1169c3ed52aef9c41e1bdcecb685bd26
Eddie <eduard.raducanu09@gmail.com>
2023-01-04 00:01:13 +0200

    minor improvements
    deleted empty rows

--- game.js
+++ game.js
@@ -3,15 +3,13 @@ var gamePattern =[];
 var userClickedPattern = [];
 var gameStart = false;
 var level = 0;
-
-$(document).keypress(function(){
+$(document).on("click", function(){
     if (gameStart === false){
         $("#level-title").text("level " + level)
         nextSequence();
         gameStart = true;
         }
     })
-
 function gameOver() {
     audio = new Audio("sounds/wrong.mp3")
     audio.play();
@@ -20,13 +18,11 @@ function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
-
 function startOver(){
     level = 0;
     gamePattern= [];
     gameStart = false;
 }
-
 $(".btn").on("click", function (){ 
     var userChosenColor = this.id;
     userClickedPattern.push(userChosenColor);
@@ -34,7 +30,6 @@ $(".btn").on("click", function (){
     animatePress(userChosenColor);
     checkAnswer(userClickedPattern.length-1);
 });
-
 function checkAnswer(currentLevel){
     if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
         console.log("ok");
@@ -53,33 +48,19 @@ function checkAnswer(currentLevel){
         startOver();
     }
 }
-
 function nextSequence() {
     userClickedPattern = [];
     level++;  
     $("#level-title").text("level " + level)
-
-    //creating a variable that stores a random number from 0 -> 3
     var randomNumber = Math.floor(Math.random()*4);
-    
-    //variable that stores the random color based on the random number
     var randomChosenColor = buttonColors[randomNumber];
-    
-    //storing the random color chosen in the empty array 
     gamePattern.push(randomChosenColor);
-    
-    //alert when a button is pressed
     $("." + randomChosenColor).fadeOut(100).fadeIn(100);
-    
-    //calling the playSound function of the random color generated
     playSound(randomChosenColor)
-
 }
-
 function animatePress (currentColor){
     $("#" + currentColor).addClass("pressed");
     setTimeout(function() {
         $("#" + currentColor).removeClass("pressed");
     }, 100)
 }
-
